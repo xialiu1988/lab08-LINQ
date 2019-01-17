7562019685b04f7f2eba3de49481aad6ea11230d
xia LIU <xia.liu2@outlook.com>
2019-01-17 12:25:32 -0800

    created a class called properties

--- lab8-LINQ/lab8-LINQ/Program.cs
+++ lab8-LINQ/lab8-LINQ/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace lab8_LINQ
 {
---
+++ lab8-LINQ/lab8-LINQ/Properties.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab8_LINQ
+{
+   public class Properties
+    {
+
+
+        public string zip { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string address { get; set; }
+        public string borough { get; set; }
+        public string neighborhood { get; set; }
+        public string county { get; set; }
+
+
+    }
+}
